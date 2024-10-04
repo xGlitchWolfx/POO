cddaa0b731ecb742e5e8592c58622bac7954cd3d
Cambio1 <uspwolf2563@gmail.com>
2024-10-04 12:55:03 -0500

    Cambio1

--- src/main/java/org/example/TrabajoEnClase.java
+++ src/main/java/org/example/TrabajoEnClase.java
@@ -5,5 +5,7 @@ public class TrabajoEnClase {
         System.out.println("Nombre: Kevin Chacon");
         System.out.println("Telefono: 0983794446");
         System.out.println("Direccion: El pedestal");
+        System.out.println("Me olvide!");
+        System.out.println("Soy dibujante y programador! ahora si gracias por su atencion");
     }
 }
