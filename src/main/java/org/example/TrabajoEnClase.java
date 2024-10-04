a208f122efcb7c4e64afe307f7a1a2ee7e5d0c41
Cambio1 <uspwolf2563@gmail.com>
2024-10-04 12:53:35 -0500

    Cambio1

---
+++ src/main/java/org/example/TrabajoEnClase.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class TrabajoEnClase {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Kevin Chacon");
+        System.out.println("Telefono: 0983794446");
+        System.out.println("Direccion: El pedestal");
+    }
+}
