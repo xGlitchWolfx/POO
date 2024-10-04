89c07206672f5a9f8dfd5807900dcbc3ead7d75d
Cambio1 <uspwolf2563@gmail.com>
2024-10-04 12:16:03 -0500

    Cambio 2

--- src/main/java/org/example/Ejercicio2.java
+++ src/main/java/org/example/Ejercicio2.java
@@ -3,5 +3,6 @@ package org.example;
 public class Ejercicio2 {
     public static void main(String[] args) {
         System.out.println("hola Mundo");
+        System.out.println("Hola Mundito");
     }
 }
