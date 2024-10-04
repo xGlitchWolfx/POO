e549e326e65614feffb170f57dac45e822c5e429
Cambio1 <uspwolf2563@gmail.com>
2024-10-04 12:03:11 -0500

    Cambio 2

---
+++ src/main/java/org/example/Ejercicio1.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int edad = 18;
+
+        if (edad >=18){
+            System.out.println("Usted puede realizar el proceso de votaciones en el ecuador");
+        }
+
+            System.out.println("PRIMERA PRACTICA");
+            System.out.println("Hola Github");
+            System.out.println("Hola Mundito");
+    }
+}
